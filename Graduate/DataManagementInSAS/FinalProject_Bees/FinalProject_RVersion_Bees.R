0ab5a018313d7543439007878fb5587a23a90b3a
HXL <lawshx.appstate.edu>
2019-04-26 10:12:17 -0400

    changesWithSASFiles

---
+++ Graduate/DataManagementInSAS/FinalProject_Bees/FinalProject_RVersion_Bees.R
@@ -0,0 +1,21 @@
+actives <- read.csv("FaithYard/actives")
+additional_counts <- read.csv("FaithYard/additional_counts")
+all_yard_counts <- read.csv("FaithYard/all_yard_counts")
+ar_internal_metadata <- read.csv("FaithYard/ar_internal_metadata")
+# dispatches <- read.csv("FaithYard/dispatches") #empty
+feed_lists <- read.csv("FaithYard/feed_lists")
+forms <- read.csv("FaithYard/forms", sep = ";")
+groupings <- read.csv("FaithYard/groupings")
+groups <- read.csv("FaithYard/groups")
+# hives <- read.csv("FaithYard/hives") #empty
+medicate_lists <- read.csv("FaithYard/medicate_lists")
+roles <- read.csv("FaithYard/roles")
+# scales <- read.csv("FaithYard/scales") # empty
+schema_migrations <- read.csv("FaithYard/schema_migrations")
+# tasks <- read.csv("FaithYard/tasks") #empty
+# todolists <- read.csv("FaithYard/todolists") # empty
+users <- read.csv("FaithYard/users")
+users_roles <- read.csv("FaithYard/users_roles")
+yard_states <- read.csv("FaithYard/yard_states", sep = ";")
+yards <- read.csv("FaithYard/yards")
+
