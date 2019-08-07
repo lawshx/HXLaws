3c23030596aee2536df32c317142e617edd3548b
lawshx <lawshx@appstate.edu>
2019-08-07 19:41:01 -0400

    b

--- Side_Projects/weather.py
+++ Side_Projects/weather.py
@@ -1,13 +1,22 @@
+import re
 import requests
-req = requests.get('https://www.wunderground.com/forecast/us/nc/charlotte/KCLT?cm_ven=localwx_10day')
-
 from bs4 import BeautifulSoup as BS
-soup = BS(req.text, features = "html.parser")
-s = soup.find_all('script')
-cdata = s[5]
-#print(cdata)
+#Request to get information from weather underground
+#req = requests.get('https://www.wunderground.com/forecast/us/nc/charlotte/KCLT?cm_ven=localwx_10day')
 
-r = requests.get('https://forecast.weather.gov/MapClick.php?lat=36.2051&lon=-81.665&lg=english&&FcstType=digital')
-print(r.text)
+
+#request to get weather data from forecast.weather.gov
+r = requests.get('https://forecast.weather.gov/MapClick.php?lat=36.2051&lon=-81.6658&lg=english&&FcstType=digitalDWML')
+
+#Parsing as HTML
 soup = BS(r.text,features = 'html.parser')
-print(soup.option)
+#print(soup.prettify())
+
+#finding and storing the necessary raw information
+date = soup.find_all('time-layout')
+HI = soup.find_all('temperature', type = re.compile('heat index'))
+hourly_temp = soup.find_all('temperature', type = 'hourly')
+wind = soup.find_all('direction', type = 'wind')
+hourly_precip = soup.find_all('hourly-qpf', type = 'floating')
+print(hourly_precip)
+print(type(hourly_precip))
