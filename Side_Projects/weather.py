0c82473e95afec311928ac800f46a904904352c2
lawshx <lawshx@appstate.edu>
2019-08-07 18:40:30 -0400

    b

--- Side_Projects/weather.py
+++ Side_Projects/weather.py
@@ -5,4 +5,9 @@ from bs4 import BeautifulSoup as BS
 soup = BS(req.text, features = "html.parser")
 s = soup.find_all('script')
 cdata = s[5]
-print(cdata)
+#print(cdata)
+
+r = requests.get('https://forecast.weather.gov/MapClick.php?lat=36.2051&lon=-81.665&lg=english&&FcstType=digital')
+print(r.text)
+soup = BS(r.text,features = 'html.parser')
+print(soup.option)
