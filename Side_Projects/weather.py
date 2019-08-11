c2f8fcc6eddaafd35adc6553c9e02b4799a7aab4
HXL <lawshx.appstate.edu>
2019-08-11 14:08:36 -0400

    extracting more info

--- Side_Projects/weather.py
+++ Side_Projects/weather.py
@@ -2,6 +2,7 @@ import re
 import requests 
 from bs4 import BeautifulSoup as BS 
 import pandas as pd
+import types
 #from lxml import html
 #Request to get information from weather underground
 #req = requests.get('https://www.wunderground.com/forecast/us/nc/charlotte/KCLT?cm_ven=localwx_10day')
@@ -20,34 +21,70 @@ soup = BS(r.text,features = 'html.parser')
 #date by the hour
 date = soup.find('time-layout').select('start-valid-time')
 
+#dew point
+dew = soup.find('temperature', type = re.compile('dew point')).select('value')
+
 #Heat Index 
 HI = soup.find('temperature', type = re.compile('heat index')).select('value')
 
 #hourly temperature
-hourly_temp = soup.find('temperature').select('value')
+hourly_temp = soup.find('temperature', type = 'hourly').select('value')
+
+#wind speed
+wind_speed = soup.find('wind-speed', type = re.compile('sustained')).select('value')
+#gusts wind speed 
+wind_gust = soup.find('wind-speed', type = 'gust').select('value')
 
 #wind direction in degrees
-wind = soup.find('direction').select('value')
+wind_dir = soup.find('direction').select('value')
 
-#precipitation by the hour
+#cloud amount in percentage
+cloud = soup.find('cloud-amount', type = 'total').select('value')
+
+#chance of precipitation
+precip_pred = soup.find('probability-of-precipitation').select('value')
+
+#percentage humidity
+humidity = soup.find('humidity').select('value')
+
+#precipitation by the hour in inches
 hourly_precip = soup.find('hourly-qpf').select('value')
 
+#weather condition: ie rain or thunderstorm
+#needs a little more work to extract the weather-type
+weather_cond = soup.find('weather').select('weather-conditions')
 
-variables = [date, HI, hourly_temp, hourly_precip, wind]
+for i in range(len(weather_cond)):
+    if type(weather_cond[25].value) == type(weather_cond[26].value):
+        
+
+
+(soup.find('weather').find_all('weather-conditions')[25]).find_all('value')[1]['weather-type']
+
+#listing all the variables in a list
+variables = [date, HI, weather_cond, hourly_temp, dew, precip_pred, hourly_precip, humidity, cloud, wind_gust, wind_speed, wind_dir]
 for i in range(len(variables)):
-    for j in range(len(date)):
+    print(len(variables[i]))
+
+
+#Going through each variable, replacing the value with the tags with the value WITHOUT the tags
+for i in range(len(variables)):
+    for j in range(len(variables[i])):
         a = (variables[i])[j]
         (variables[i])[j] = (variables[i])[j].get_text()
 
 
+#Creating dataframe to store information
 WeatherData = pd.DataFrame([date])
-WeatherData = WeatherData.append([HI])
-WeatherData = WeatherData.append([hourly_temp])
-WeatherData = WeatherData.append([hourly_precip])
-WeatherData = WeatherData.append([wind])
+for i in range(1,len(variables)):
+    WeatherData = WeatherData.append([variables[i]])
 
-tidyWeather = WeatherData.transpose()
-tidyWeather.columns = ['DateTime','Heat Index', 'HrlyTemp', 'HrlyPrecip', 'Wind']
 
+#transpose information so that it's every row is an observation
+tidyWeather = WeatherData.transpose() 
+#Changing column names
+tidyWeather.columns = ['DateTime','Heat Index', 'WeatherCond', 'HrlyTemp', 'DewPt', 'PrecipPredict','HrlyPrecip', 'Humidity', 'CloudCover', 'WindGust', 'WindSpeed', 'WindDir']
+
+#Testing out results
 print(WeatherData[7])
 print(tidyWeather[0:20])
