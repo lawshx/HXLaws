086c927616d4ce4d740b75d7cd935cc4b33a52f4
H X Laws <lawshx@gmail.com>
2020-07-17 21:43:03 +0000

    Add Python Projects

---
+++ Side_Projects/Python/practice_processing.py
@@ -0,0 +1,11 @@
+#Python 3.8.2
+#This script is to focus on using the csv and file tools to process data without needing to use a majority of the RAM.
+#First will probably need to grab a large data set
+#And also import a couple of things
+
+import pandas as pd
+import csv
+import os
+import datetime
+
+os.chdir(r'/home/lawshx/Downloads') 
